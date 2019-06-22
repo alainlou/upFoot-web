0737574f9ed44b277a004db503f7032d7ff12bbf
alainlou <az2lou@uwaterloo.ca>
2019-06-22 00:08:41 -0400

    rename

--- functions/verify.js
+++ functions/verify.js
@@ -5,8 +5,13 @@ const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 */
 module.exports = async (context) => {
 
-  console.log(context.params);
+  let username = context.params['user'];
+  let verified = context.params['verified']
 
+  let tmp = context.http.headers;
+  
+  console.log(tmp);
+  
   // Prepare workflow object to store API responses
   
   let workflow = {};
@@ -17,7 +22,7 @@ module.exports = async (context) => {
   
   workflow.response = await lib.slack.channels['@0.4.18'].messages.create({
     channel: `#fingerprint-demo`,
-    text: `Hello from stdlib!`,
+    text: username + ` has entered the building.`,
     attachments: null
   });
 
