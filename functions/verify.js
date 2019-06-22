ffff1c5e0dc0b06d76671f0d522a82707dc65f74
alainlou <az2lou@uwaterloo.ca>
2019-06-22 00:37:37 -0400

    EOD updates

--- functions/verify.js
+++ functions/verify.js
@@ -6,11 +6,16 @@ const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 module.exports = async (context) => {
 
   let name = context.params['name'];
-  let verified = context.params['verified']
+  let verified = context.params['verified'];
 
-  let tmp = context.http.headers;
+  console.log(context.params);
+  console.log(verified);
+
+  /**
+   * let name = context.http.headers['x-name']
+   * let verified = context.http.headers['x-verified']
+   */
   
-  console.log(tmp);
   
   // Prepare workflow object to store API responses
   
@@ -19,10 +24,18 @@ module.exports = async (context) => {
   // [Workflow Step 1]
   
   console.log(`Running slack.channels[@0.4.18].messages.create()...`);
+
+  // Setting the message to display in Slack
+  let message = name + ` was `;
+  if(verified === true) {
+    message = message + `verified.`;
+  } else {
+    message = message + `not verified.`;
+  }
   
   workflow.response = await lib.slack.channels['@0.4.18'].messages.create({
     channel: `#fingerprint-demo`,
-    text: name + ` has entered the building.`,
+    text: message,
     attachments: null
   });
 
